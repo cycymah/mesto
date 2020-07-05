cae40a00a7927c8314d1dffcbda550848f76388e
Ivan Solnyshkin <ethosy@yandex.ru>
2020-07-05 11:04:14 +0700

    Тест

--- script.js
+++ script.js
@@ -22,27 +22,27 @@ function modalOff() {
 
 profileEdit.addEventListener('click', modalOn);
 formClose.addEventListener('click', modalOff);
-// // Находим форму в DOM
-// let formElement = // Воспользуйтесь методом querySelector()
+// Находим форму в DOM
+let formElement = // Воспользуйтесь методом querySelector()
 
-// // Обработчик «отправки» формы, хотя пока
-// // она никуда отправляться не будет
-// function formSubmitHandler (evt) {
-//     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-//                         // Так мы можем определить свою логику отправки.
-//                         // О том, как это делать, расскажем позже.
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                        // Так мы можем определить свою логику отправки.
+                        // О том, как это делать, расскажем позже.
 
-//     // Находим поля формы в DOM
-//     let nameInput = // Воспользуйтесь инструментом .querySelector()
-//     let jobInput = // Воспользуйтесь инструментом .querySelector()
+    // Находим поля формы в DOM
+    let nameInput = // Воспользуйтесь инструментом .querySelector()
+    let jobInput = // Воспользуйтесь инструментом .querySelector()
 
-//     // Получите значение полей из свойства value
+    // Получите значение полей из свойства value
 
-//     // Выберите элементы, куда должны быть вставлены значения полей
+    // Выберите элементы, куда должны быть вставлены значения полей
 
-//     // Вставьте новые значения с помощью textContent
-// }
+    // Вставьте новые значения с помощью textContent
+}
 
-// // Прикрепляем обработчик к форме:
-// // он будет следить за событием “submit” - «отправка»
-// formElement.addEventListener('submit', formSubmitHandler);
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
