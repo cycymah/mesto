f669415a79a08db368c95daa05f84afe8d606b5f
Ivan Solnyshkin <ethosy@yandex.ru>
2020-07-05 23:11:50 +0700

    fix: Добработаны эл-ты сайта и скрит submit

--- script.js
+++ script.js
@@ -3,18 +3,20 @@ const profileEdit = document.querySelector('.profile__edit-btn');
 const formShow = document.querySelector('.modal');
 const formClose = document.querySelector('.form__close-btn');
 
+
+
 function modalOn() {
   let profileName = document.querySelector('.profile__title-name');
   let inputProfile = document.querySelector('.form__input_name');
   let profileAbout = document.querySelector('.profile__subtitle-name');
   let inputAbout = document.querySelector('.form__input_about');
 
+
   inputProfile.value = profileName.textContent;
   inputAbout.value = profileAbout.textContent;
   formShow.classList.add('modal_activ');
 
 };
-
 
 function modalOff() {
   formShow.classList.remove('modal_activ');
@@ -23,8 +25,8 @@ function modalOff() {
 profileEdit.addEventListener('click', modalOn);
 formClose.addEventListener('click', modalOff);
 // Находим форму в DOM
-let formElement = // Воспользуйтесь методом querySelector()
-
+// Воспользуйтесь методом querySelector()
+let formElement = document.querySelector('.form__section');
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler (evt) {
@@ -33,16 +35,24 @@ function formSubmitHandler (evt) {
                         // О том, как это делать, расскажем позже.
 
     // Находим поля формы в DOM
-    let nameInput = // Воспользуйтесь инструментом .querySelector()
-    let jobInput = // Воспользуйтесь инструментом .querySelector()
+    let profileName = document.querySelector('.profile__title-name');
+    let profileAbout = document.querySelector('.profile__subtitle-name');
+    let inputProfile = document.querySelector('.form__input_name');
+    let inputAbout = document.querySelector('.form__input_about');
 
     // Получите значение полей из свойства value
-
+    let profileNameValue = inputProfile.value;
+    let profileAboutValue = inputAbout.value;
+    console.log(profileNameValue);
     // Выберите элементы, куда должны быть вставлены значения полей
-
+    profileName.textContent = profileNameValue;
+    profileAbout.textContent = profileAboutValue;
     // Вставьте новые значения с помощью textContent
+    console.log('Form');
+    
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener('submit', modalOff);
