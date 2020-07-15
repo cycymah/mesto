beea41313658e0362233a2d108f0901aa25267f7
Ivan Solnyshkin <ethosy@yandex.ru>
2020-07-15 20:58:48 +0700

    feat: Добавлено окно add-button, события закрытия окна и добавление карточек через template

--- js/script.js
+++ js/script.js
@@ -1,29 +1,103 @@
 
-const profileEdit = document.querySelector('.profile__edit-btn');
-const formShow = document.querySelector('.modal');
-const formClose = document.querySelector('.form__close-btn');
-const profileName = document.querySelector('.profile__title-name');
+//Модалки
+const profileShow = document.querySelector('.modal_target_profile');        //Модалка с профайлом
+const addShow = document.querySelector('.modal_target_addCard');            //Модалка с карточками
+
+//Кнопки
+const profileEdit = document.querySelector('.profile__edit-btn');           //Кнопка редактирования профайла
+const formClose = document.querySelector('.form__close-btn_target_profile');//Кнопка закрытия профала
+const addButton = document.querySelector('.profile__add-button');           //Кнопка добавления карточки 
+const formCloseAdd = document.querySelector('.form__close-btn_target_add'); //Кнопка закрытия модалки с карточками
+
+//Текстовый контент
+const profileName = document.querySelector('.profile__title-name');         
 const profileAbout = document.querySelector('.profile__subtitle-name');
+
+//Формы
+const formElement = document.querySelector('.form__section');
+
+//Инпуты
 const inputProfile = document.querySelector('.form__input_field_name');
 const inputAbout = document.querySelector('.form__input_field_about');
 
+//Отдельные блоки
+const list = document.querySelector('.elements__list');                     //Список элементов
+//Массив карточек
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
+// function addListCard () {
+//     for (let i = 0; i < initialCards.length; i++) {
+//       list.append(listElement);
+      
+//   }
+// }
+// window.onload = function () {
+//   list.append(listElement);
+// }
 
-function modalOn() {
+//Добавление элементов в список
+
+function addListItem () {
+  const listTemplate = document.querySelector('#listItem').content;
+  for (let i = 0; i < initialCards.length; i++) {
+    const listElement = listTemplate.cloneNode(true);  
+    list.append(listElement);
+  }
+};
+
+window.onload = addListItem();
+
+//Открытие-закрытие модалок
+function profileOn() {
   inputProfile.value = profileName.textContent;
   inputAbout.value = profileAbout.textContent;
-  formShow.classList.add('modal_activ');
+  profileShow.classList.add('modal_activ');
 };
+
+function addCardOn() {
+  addShow.classList.add('modal_activ');
+}
 
 function modalOff() {
-  formShow.classList.remove('modal_activ');
+  addShow.classList.remove('modal_activ');
+  profileShow.classList.remove('modal_activ');
 };
 
-profileEdit.addEventListener('click', modalOn);
-formClose.addEventListener('click', modalOff);
+//Удаление элементов (корзина)
+const trashButton = document.querySelector('.elements__trash');             //Корзина
 
-const formElement = document.querySelector('.form__section');
+function itemTrash(evt) {
+  const listItem = trashButton.closest('.elements__item');
+  listItem.remove();
+  console.log('!!!');
+}
 
+//Закрытие с соханиением
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = inputProfile.value;;
@@ -31,6 +105,12 @@ function formSubmitHandler (evt) {
     modalOff();
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
+//События
+formElement.addEventListener('submit', formSubmitHandler);  //Добавление инфы в профайл
+profileEdit.addEventListener('click', profileOn);           //Открытие формы профайла
+addButton.addEventListener('click', addCardOn);             //Открытие формы добавления карточек
+formClose.addEventListener('click', modalOff);              //Закрытие формы профайла
+formCloseAdd.addEventListener('click', modalOff);           //Закрытие формы добавления карточек
+trashButton.addEventListener('click', itemTrash(evt));      //Удаление в корзину
 
-//Спасибо за ревью! Счастья, печенек и хорошего настроения!:)
+//При загрузке страницы
