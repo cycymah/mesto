aea2ba2fa2b05a650a5fbf1c17864864b2a39f16
Ivan Solnyshkin <ethosy@yandex.ru>
2020-08-11 15:44:06 +0700

    fix: Исправлено отображение картинок

--- js/initial-cards.js
+++ js/initial-cards.js
@@ -1,33 +1,33 @@
 //Массив карточек
 const initialCards = [{
   name: 'Алтай',
-  link: '../images/places/altay.jpeg',
+  link: './images/places/altay.jpeg',
   alt: 'Старинный мост через р. Катунь'
 },
 {
   name: 'Барнаул',
-  link: '../images/places/barnaul.jpeg',
+  link: './images/places/barnaul.jpeg',
   alt: 'Вид на город Барнаул с обзорной площадки'
 
 },
 {
   name: 'Озеро Белё',
-  link: '../images/places/bele.jpeg',
+  link: './images/places/bele.jpeg',
   alt: 'Вид на озеро Белё на фоне палатки'
 },
 {
   name: 'р. Енисей',
-  link: '../images/places/enisey.jpeg',
+  link: './images/places/enisey.jpeg',
   alt: 'Солнечные песочные пляжи на р. Енисей'
 },
 {
   name: 'Игарка',
-  link: '../images/places/igarka.jpeg',
+  link: './images/places/igarka.jpeg',
   alt: 'Енисей во льду вблизи города Игарка'
 },
 {
   name: 'Тура',
-  link: '../images/places/nt.jpeg',
+  link: './images/places/nt.jpeg',
   alt: 'Зимник по р. Нижняя тунгуска вблизи г. Тура'
 }
 ];
